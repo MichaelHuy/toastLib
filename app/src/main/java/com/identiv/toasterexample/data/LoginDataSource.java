d61a97fbda0a488289e7bc1775153aa341503ca4
Huy Le <lehuydit@gmail.com>
2021-05-21 10:45:57 +0700

    first commit

---
+++ app/src/main/java/com/identiv/toasterexample/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.identiv.toasterexample.data;
+
+import com.identiv.toasterexample.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
